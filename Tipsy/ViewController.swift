ebb4dc4b3c57becbff73332836714246d7aec601
t9guan <50217057+t9guan@users.noreply.github.com>
2020-01-11 18:54:44 -0800

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Tony Guan on 1/11/20.
+//  Copyright © 2020 Tony Guan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
